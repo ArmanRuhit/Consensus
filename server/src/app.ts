f847e44aa65fb0c614cd83b87a07d5bd98401276
Ruhit Arman <armanruhit09@gmail.com>
2026-05-15 23:51:23 +0600

    serve built client static files from Express for production deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,15 @@ import { env } from "./config/env.js";
 import express from "express";
 import cors from "cors"
 import cookieParser from 'cookie-parser'
+import path from "path";
+import { fileURLToPath } from "url";
 import swaggerUi from "swagger-ui-express";
 import { openapiSpec } from "./config/openapi.js";
 import { router } from './routes/index.js'
 import { errorMiddleWare } from "./middleware/error.middleware.js";
 import { notFoundMiddleWare } from "./middleware/notFound.middleware.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express()
 
@@ -25,6 +29,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec))
 app.get('/api-spec.json', (_req, res) => res.json(openapiSpec))
 
 app.use('/api', router)
+
+const clientDist = path.resolve(__dirname, "../../client/dist");
+app.use(express.static(clientDist))
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(clientDist, "index.html"))
+})
 
 app.use(notFoundMiddleWare)
 app.use(errorMiddleWare)
